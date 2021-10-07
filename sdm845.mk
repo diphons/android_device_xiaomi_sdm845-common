d9b766c6b31135f8ac3d5bfd05af9ac955031014
Pavel Dubrova <pashadubrova@gmail.com>
2021-10-07 10:39:12 +0300

    sdm845-common: Provide android.frameworks.sensorservice@1.0 on vendor
    Following the commit f19703e ("common-treble: Provide radio, keymaster
    and secure_element HAL on vendor"), starting with Android 12 most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        QCamera : <MCI><ERROR> mm_camera_load_shim_lib: 3306: dlopen failed
        with error dlopen failed: library
        "android.frameworks.sensorservice@1.0.so" not found: needed by
        /odm/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    android.frameworks.sensorservice@1.0 is used by at least mm-camera
    framework.
    
    Needed by slim_daemon (gps daemon as well in U)
    10-08 09:41:12.189  1473  1473 F linker  : CANNOT LINK EXECUTABLE "slim_daemon": library "android.frameworks.sensorservice@1.0.so" not found: needed by main executable
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -343,6 +343,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 # Speed profile services and wifi-service to reduce RAM and storage.
