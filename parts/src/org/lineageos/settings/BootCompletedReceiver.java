3073ade2fe07aff90bcaeaa3fbf58e2ab81e3adb
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-15 13:31:22 +0700

    sdm845-common: parts: Use proper singleton pattern for DiracUtils
    No memory leak caused by static field context anymore.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -35,8 +35,13 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize();
-        DozeUtils.checkDozeService(context);
+        try {
+            DiracUtils.getInstance(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
+
+	DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
 }
