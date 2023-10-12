a71d87c6db49a7184744c5ed95299e8ec184c284
Aman Singh <amankumarsingh1412@gmail.com>
2023-10-12 17:21:45 +0000

    sdm845-common: Patch widevine to load libprotobuf-cpp-full-3.9.1 * Patch widevine to load full variant as there's no lite vendorcompat variant

--- sdm845.mk
+++ sdm845.mk
@@ -302,6 +302,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
