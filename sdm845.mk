8c7ef852fd62c645aa1a51ae2e820c72fe45a7ab
Rudi Setiyawan <diphons@gmail.com>
2024-03-05 07:48:51 +0700

    Revert "sdm845-common: Build system vendor.qti.hardware.display.config-V5-ndk from source for wfd stack"
    This reverts commit 5e796e05001cab7e5fd96fb4a2a06b2d23b3ceca.

--- sdm845.mk
+++ sdm845.mk
@@ -431,8 +431,7 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
-    libwfdaac_vendor \
-    vendor.qti.hardware.display.config-V5-ndk
+    libwfdaac_vendor
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
